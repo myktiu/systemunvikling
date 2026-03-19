e6c413a4bd21db921d2c602ab9f74e977a1b1272
erlinwe <erlinwe@stud.ntnu.no>
2026-03-20 00:49:05 +0100

    MVP 0.2b

--- systemutv/src/main/java/org/example/login.java
+++ systemutv/src/main/java/org/example/login.java
@@ -1,13 +1,38 @@
 /**
  * Author: Erling Wendt
- * Version: MVP 0.1
- * 18/03/2026
+ * Version: MVP 0.2
+ * 19/03/2026
  *
  * This class should create a way for a user to log in, I plan to add a username, and a hashed password.
  * It should directly connect to a database, so that username and password can be checked to see if
  * they fit with our systems
  */
 
+package org.example;
 
-public class login {
+public class Login {
+
+    public userInfo authenticate(String username, String password) {
+        String hashed = passwordEncryption.hashPassword(password);
+
+        // TODO: bytt ut placeholder names her Rohith
+        // DBConnection db = DBConnection.getInstance();
+        // if (!db.verifyUser(username, hashed)) return null;
+        // UserInfo user = new UserInfo();
+        // user.setUserID(db.getUserId(username));
+        // user.setUsername(username);
+        // user.setDonationHistory(Donation.getDonationsByUser(user.getUserID()));
+        // return user;
+        // TEMP mock - fjern når Rohith er klar
+        UserInfo mockUser = new UserInfo();
+        mockUser.setPassword("1234"); // hashed automatically inside setPassword
+
+        if (username.equals("test") && hashed.equals(mockUser.getPassword())) {
+            mockUser.setUserID(1);
+            mockUser.setUsername(username);
+            mockUser.setDonationHistory(Donation.getDonationsByUser(1));
+            return mockUser;
+        }
+        return null;
+    }
 }
--- systemutv/src/main/java/org/example/userInfo.java
+++ systemutv/src/main/java/org/example/userInfo.java
@@ -1,10 +1,36 @@
 /**
  * Author: Erling Wendt
- * Version: MVP 0.1
- * 18/03/2026
+ * Version: MVP 0.2
+ * 19/03/2026
  * This class should allow for a showcase of user info, so that a user can see their own donation history,
  * and potentially change usernames or passwords
  */
+package org.example;
+import java.util.List;
+
+
 
 public class userInfo {
+    private int userID;
+    private String username;
+    private List<Donation> donationHistory;
+    private String password;
+
+    public int  getUserID() {return userID;}
+    public void setUserID(int userID) {this.userID = userID;}
+
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String newPassword) {
+        this.password = passwordEncryption.hashPassword(newPassword);
+    }
+
+    public List<Donation> getDonationHistory() {return this.donationHistory;}
+    public void setDonationHistory(List<Donation> donationHistory) {
+        this.donationHistory = donationHistory;
+    }
+    //TODO: I need a way to upload a set detail about a user to the DB. Called something like sendToDB().
+    //TODO: Honestly it should be its own class that can be called upon whenever we need.
 }
